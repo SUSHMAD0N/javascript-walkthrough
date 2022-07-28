4ce7a358ed88e0f5f7a10880192ef4fe586f9d9f
Sushma D N <sushma.dn@mail.waldenu.edu>
2022-07-28 12:57:31 +0530

    commit -3

---
+++ es6/arroe-function.js
@@ -0,0 +1,45 @@
+//ES - 6
+odds  = evens.map(v => v + 1)
+pairs = evens.map(v => ({ even: v, odd: v + 1 }))
+nums  = evens.map((v, i) => v + i)
+
+nums.forEach(v => {
+    if (v % 5 === 0)
+        fives.push(v)
+ })
+
+ this.nums.forEach((v) => {
+    if (v % 5 === 0)
+        this.fives.push(v)
+})
+
+
+//ES-5
+odds  = evens.map(function (v) { return v + 1; });
+pairs = evens.map(function (v) { return { even: v, odd: v + 1 }; });
+nums  = evens.map(function (v, i) { return v + i; });
+
+nums.forEach(function (v) {
+    if (v % 5 === 0)
+        fives.push(v);
+ });
+
+
+ //  variant 1
+var self = this;
+this.nums.forEach(function (v) {
+    if (v % 5 === 0)
+        self.fives.push(v);
+});
+
+//  variant 2
+this.nums.forEach(function (v) {
+    if (v % 5 === 0)
+        this.fives.push(v);
+}, this);
+
+//  variant 3 (since ECMAScript 5.1 only)
+this.nums.forEach(function (v) {
+    if (v % 5 === 0)
+        this.fives.push(v);
+}.bind(this));
---
+++ es6/block-scoped-functions.js
@@ -0,0 +1,22 @@
+{
+    function foo () { return 1 }
+    foo() === 1
+    {
+        function foo () { return 2 }
+        foo() === 2
+    }
+    foo() === 1  
+}                                           
+
+//ES 5
+//  only in ES5 with the help of block-scope emulating
+//  function scopes and function expressions
+(function () {
+    var foo = function () { return 1; }
+    foo() === 1;
+    (function () {
+        var foo = function () { return 2; }
+        foo() === 2;
+    })();
+    foo() === 1;
+})()
